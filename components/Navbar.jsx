0c88fd4673386fa22d74a680be6d0ff5ac86bbfd
Tetsuki73 <abhinandanbiswal16rr@gmail.com>
2025-08-14 14:44:59 +0530

    minor 7.5.5

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
             <li><a className='font-Ovo' href="#about">About Us</a></li>
             <li><a className='font-Ovo' href="#services">Services</a></li>
             <li><a className='font-Ovo' href="#work">Why Us</a></li>
-            <li><a className='font-Ovo' href="#contact">Contact me</a></li>
+            <li><a className='font-Ovo' href="#contact">Contact Us</a></li>
         </ul>
 
         <div className='flex items-center gap-4'>
